be0fc9716321e1f1ae6c76ad1f19a628f45a8dfd
MaxStepanov90 <MAXSTEPANOV90@mail.ru>
2019-10-14 23:23:59 +0300

    Commit 1

---
+++ src/main/java/domain/Domain.java
@@ -0,0 +1,52 @@
+package domain;
+
+import entity.Address;
+import entity.Employee;
+import entity.Project;
+import org.hibernate.Session;
+import utils.HibernateUtil;
+
+import java.util.*;
+
+public class Domain {
+    public static void main(String[] args) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        Address address = new Address();
+        address.setCountry("DC");
+        address.setCity("Gotham");
+        address.setStreet("Arkham street 1");
+        address.setPostCode("0987");
+
+        Project project = new Project();
+        project.setTitle("Gotham PD");
+
+        Employee employee = new Employee();
+        employee.setFirstname("John");
+        employee.setLastname("Gordon");
+
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(1939,Calendar.MAY,1);
+
+        employee.setBirthday(new Date(calendar.getTime().getTime()));
+        employee.setAddress(address);
+
+        Set<Employee> employees = new HashSet<>();
+        employees.add(employee);
+        project.setEmployees(employees);
+
+        Set<Project> projects = new HashSet<>();
+        projects.add(project);
+        employee.setProjects(projects);
+
+        session.save(address);
+        session.save(employee);
+        session.save(project);
+
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
+    }
+}
